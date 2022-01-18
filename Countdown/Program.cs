cd0608ec49c4236d55c74fa071ae914c2ad0e1d8
BearintheWillows <sjwillows91@outlook.com>
2022-01-18 23:39:21 +0000

    Completed two new challenges:
    - Countdown (Using recursion)
    - The Replicator of D'tor

---
+++ Countdown/Program.cs
@@ -0,0 +1,20 @@
+﻿// int Factorial(int number)
+// {
+//     if (number == 1) return 1;
+//     return number * Factorial(number - 1);
+// }
+
+/* Objectives:
+• Write code that counts down from 10 to 1 using a recursive method.
+• Hint: Remember that you must have a base case that ends the recursion and that every time you
+call the method recursively, you must be getting closer and closer to that base case.*/
+
+int Countdown (int number)
+{
+    if (number == 0) return 1;
+    Console.WriteLine(number);
+    return Countdown(number - 1);
+    
+}
+
+Countdown(10);
---
+++ The-Replicator-of-D’To/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array1 = new int[5];
+int[] array2 = new int[5];
+int loopNum = 0;
+int element;
+
+do
+{
+    Console.WriteLine($@"Input a number ({loopNum} of 5)");
+    array1[loopNum] = int.Parse(Console.ReadLine());
+    loopNum++;
+
+} while (loopNum < 5);
+
+Array.Copy(array1, 0, array2, 0, 5);
+
+foreach (var item in array2)
+{
+    Console.WriteLine($"New array: {item}");
+}
