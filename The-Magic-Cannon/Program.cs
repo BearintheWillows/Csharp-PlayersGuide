bd2d8d679d8ea1cd340f232fb53f714e5e143e8e
BearintheWillows <sjwillows91@outlook.com>
2022-01-18 15:04:31 +0000

    Added two challenges

---
+++ The-Magic-Cannon/Program.cs
@@ -0,0 +1,24 @@
+﻿for (int i = 1; i < 101; i++)
+{
+    if (i % 3 == 0 && i % 5 ==0)
+    {
+       Console.ForegroundColor = ConsoleColor.Gray;
+        Console.Write($"{i}: ");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("Bo");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write("om\n");
+       
+        
+    }
+    else if (i % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"{i}: Electric");
+    }
+    else if (i % 3 == 0 )
+    {   
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{i}: Fire");
+    }
+}
---
+++ The-Prototype/Program.cs
@@ -0,0 +1,43 @@
+﻿string pilotInput;
+int pilotNumber;
+string hunterInput;
+int hunterNumber;
+
+Console.WriteLine("User 1, enter a number between 0 and 100:");
+do
+{
+    pilotInput = Console.ReadLine();
+    pilotNumber = int.Parse(pilotInput);
+    if (pilotNumber > 100 || pilotNumber < 0)
+    {
+      Console.WriteLine($"{pilotNumber} is too high/low. Try again");  
+    }
+    else
+    {
+        Console.WriteLine("Succes");
+        Console.Clear();
+    }
+    
+} while (pilotNumber > 100 || pilotNumber < 0);
+
+Console.WriteLine("User 2, your turn!");
+Console.WriteLine("Guess User 1s number:");
+
+do
+{
+    hunterInput = Console.ReadLine();
+    hunterNumber = int.Parse(hunterInput);
+
+    if (hunterNumber > pilotNumber)
+    {
+        Console.WriteLine("Too high. Try again");
+    }
+    else if (hunterNumber < pilotNumber)
+    {
+        Console.WriteLine("Too low. Try again");
+    }
+    else
+    {
+        Console.WriteLine("Congrats! You've guessed correctly!");
+    }
+} while (hunterNumber != pilotNumber);
