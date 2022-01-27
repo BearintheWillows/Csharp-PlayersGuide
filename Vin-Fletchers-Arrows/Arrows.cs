0dd45e3c08d7e30926787487d96dca1edddc5a89
BearintheWillows <sjwillows91@outlook.com>
2022-01-27 03:18:00 +0000

    Changed Fields to Properties

--- Vin-Fletchers-Arrows/Arrows.cs
+++ Vin-Fletchers-Arrows/Arrows.cs
@@ -2,32 +2,29 @@ namespace Arrows;
 
 public class Arrows
 {
-    private ArrowHead _head;
-    private double _length;
-    private Fletching _fletching;
+    private ArrowHead Head {get; set;}
+    private double Length {get; set;}
+    private Fletching Fletching {get; set;}
 
-    public Arrows(string head,string fletching,int length)
+    public Arrows(ArrowHead head,Fletching fletching,int length)
     {
-        _head = (ArrowHead) Enum.Parse(typeof(ArrowHead), head, true);
-        _fletching = (Fletching) Enum.Parse(typeof(Fletching), fletching, true);
-        _length = length;
+        Head = head;
+        Fletching = fletching;
+        Length = length;
     }
-    
-    public ArrowHead getHead() => _head;
-    public double getLength() => _length;
-    public Fletching getFletching() => _fletching;
+
     public void getCost()
     {
 
-    double headCost = _head.ToString() switch
+    double headCost = Head.ToString() switch
                     {
-                        "Steel" => 10,
+                        "steel" => 10,
                         "wood" => 3,
                         "obsidian" => 5,
                         _ => 0
                     };
             
-    double fletchCost = _fletching.ToString() switch
+    double fletchCost = Fletching.ToString() switch
                 {
                     "plastic" => 3,
                     "turkeyFeather" => 5,
@@ -35,14 +32,14 @@ public class Arrows
                     _ => 0
                 };
             
-    double lengthCost = _length * 0.05;
+    double lengthCost = Length * 0.05;
     
     double totalCost = headCost + fletchCost + lengthCost;
         
     Console.WriteLine($@"Your arrow is as follows:
-    {_head} - {headCost} gold
-    {_fletching} - {fletchCost} gold
-    {_length} centimetres - {lengthCost} gold
+    {Head} - {headCost} gold
+    {Fletching} - {fletchCost} gold
+    {Length} centimetres - {lengthCost} gold
     Total cost per arrow: {totalCost} gold");
 }
 }
--- Vin-Fletchers-Arrows/Program.cs
+++ Vin-Fletchers-Arrows/Program.cs
@@ -4,7 +4,6 @@ string headType = Console.ReadLine();
 string fletchType = Console.ReadLine();
 int length = int.Parse(Console.ReadLine());
 
-Arrows.Arrows set1 = new Arrows.Arrows(headType, fletchType, length);
+Arrows.Arrows set1 = new Arrows.Arrows((ArrowHead) Enum.Parse(typeof(ArrowHead), headType, true), (Fletching) Enum.Parse(typeof(Fletching), fletchType, true), length);
 
 set1.getCost();
-Console.WriteLine(set1.getHead());
