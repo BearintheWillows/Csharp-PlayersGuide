7b53f3177682ec3c59217c3aee2ad063c8206b98
BearintheWillows <sjwillows91@outlook.com>
2022-01-28 21:06:18 +0000

    Added Static and Null info folders

---
+++ Null/Program.cs
@@ -0,0 +1,20 @@
+﻿string? name = Console.ReadLine(); // "?" Means can return Null
+
+if (name != null)
+{
+    Console.WriteLine("The name is not null"); //null check
+}
+
+// null coalescing operator
+
+private string GetTopPlayerName() 
+{
+    string? name = _scoreManager?.GetScores()?[0]?.Name;
+    name ??= "(not found)";
+    return name; //No compiler warning. '??=' makes sure value present.
+
+}
+
+//null forigiving operator
+
+string message = MightReturnNullIfNegative(+10)!;
---
+++ Static/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using static System.Console;
+
+
+
+SomeClass aClass = new SomeClass();
+
+WriteLine(aClass.Number);
+
+SomeClass bClass = SomeClass.CreateClass();
+
+WriteLine(bClass.Number);
+// Normal Class
+
+public class SomeClass
+{
+    private int _number; //Own number field
+    public int Number {get;} = 5;//Property
+
+    public static SomeClass CreateClass() => new SomeClass();
+};
+
+public static class Utilities
+{
+    public static int Helper() => 4;
+    public static double HealperProp => 4.0;
+    public static int AddNumber(int a, int b) => a + b;
+}
+
