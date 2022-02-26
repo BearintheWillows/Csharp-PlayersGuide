6c517b4500b5cef2c5d23c400a8c7a9d232a3063
BearintheWillows <sjwillows91@outlook.com>
2022-02-26 06:39:06 +0000

    Refactored files

---
+++ Learning Modules/Interface/Program.cs
@@ -0,0 +1,35 @@
+﻿public enum TerrainType
+{
+    Desert, Forests, Mountains, Pastures, Fields, Hills
+}
+
+public class Level
+{
+    public int Width  { get; }
+    public int Height { get; }
+
+    public TerrainType GetTerrainAt(int row, int column)
+    {
+        
+    }
+}
+
+public interface ILevelBuilder
+{
+    public Level buildLevel(int levelNumber);
+}
+
+public class FixedLevelBuilder : ILevelBuilder
+{
+    public Level BuildLevel(int levelNumber)
+    {
+        Level level = new Level(10, 10, TerrainType.Forests);
+
+        level.SetTerrainAt(2, 4, TerrainType.Mountains);
+        level.SetTerrainAt(2, 5, TerrainType.Mountains);
+        level.SetTerrainAt(6, 1, TerrainType.Desert);
+
+        return level;
+    }
+}
+
--- Learning Modules/Structs/Program.cs
+++ Learning Modules/Structs/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Creating struct almost same as class 
+
+public struct Point
+{
+    public float X { get; }
+    public float Y { get; }
+
+    public Point(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+//Using struct very similar to a class
+
+Point p1 = new Point(2, 4);
