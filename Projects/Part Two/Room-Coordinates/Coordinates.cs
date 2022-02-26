496a0904f9caea447d3c1796506fb7c02bb660fe
BearintheWillows <sjwillows91@outlook.com>
2022-02-26 09:41:45 +0000

    Completed Room-Coordinate

---
+++ Projects/Part Two/Room-Coordinates/Coordinates.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Room_Coordinates;
+
+public struct Coordinate
+{
+    public int Row    { get; init; }
+    public int Column { get; init; }
+    
+    public Coordinate(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public static bool CheckCoord(Coordinate coordinate1, Coordinate coordinate2)
+    {
+        bool response = coordinate1.Row == coordinate2.Row + 1 
+                        || coordinate1.Row == coordinate2.Row - 1 
+                        || coordinate1.Column == coordinate2.Column + 1 
+                        || coordinate1.Column == coordinate2.Column - 1;
+
+        return response;
+    }
+}
--- Projects/Part Two/Room-Coordinates/Program.cs
+++ Projects/Part Two/Room-Coordinates/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Room_Coordinates;
+
+Coordinate coord1 = new Coordinate(3,4);
+Coordinate coord2 = new Coordinate(2,7);
+
+;
+
+Console.WriteLine(Coordinate.CheckCoord(coord1,coord2));
