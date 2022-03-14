c00269e4ac35a0aaeced685e4e0ba405a95c5ffc
BearintheWillows <sjwillows91@outlook.com>
2022-03-14 23:18:07 +0000

    Started "Better Random" challenge

--- Projects/Part Three/Better-Random/Program.cs
+++ Projects/Part Three/Better-Random/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace Better_Random;
+﻿
+
+10.0.NextDouble();
 
 public static class StringRandom
 {
-    public static string ToRandom(this string text)
+    public static void NextDouble(this double maxVal)
     {
-        
+        Random newRandom = new Random();
+        double resp = newRandom.Next(0,(int)maxVal);
+        Console.WriteLine((double)resp);
     }
 }
+
